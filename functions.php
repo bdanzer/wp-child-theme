2efe1582a03bbade58a259a8a91f721abadb4497
bdanzer <britt.danzer@gmail.com>
2018-08-24 22:55:49 -0500

    changed child theme to support extra classes with composer/vendor and hooked up with danzerpress theme

--- functions.php
+++ functions.php
@@ -1,18 +1,8 @@
 <?php
-use Roots\Sage\Assets;
-// enqueue styles for child theme
-// @ https://digwp.com/2016/01/include-styles-child-theme/
-function danzerpress_child_enqueue() {
-	
-	// enqueue parent styles
-	wp_enqueue_style('parent-theme', Assets\asset_path('styles/main.css'), false, null);
-	
-	// enqueue child styles
-	wp_enqueue_style('child-theme', get_stylesheet_directory_uri() . '/style.css', ['parent-theme']);
+$autoload_path = __DIR__ . '/vendor/autoload.php';
 
-    //child theme js
-	wp_enqueue_script('scripts', get_stylesheet_directory_uri() . '/js/dist/scripts.min.js', array(), null, true);
-
-	//google fonts
-	wp_enqueue_style('child-fonts', 'https://fonts.googleapis.com/css?family=Montserrat:400,700|Roboto:400,700');	
+if ( file_exists( $autoload_path ) ) {
+    require_once( $autoload_path );
 }
+
+new DanzerpressChild\DanzerpressChild();
