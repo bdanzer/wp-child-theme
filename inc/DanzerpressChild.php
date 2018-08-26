c7ff2c7e56702332a35d6e6c01fdff579e145736
bdanzer <britt.danzer@gmail.com>
2018-08-25 23:36:52 -0500

    added loading for different environments

--- inc/DanzerpressChild.php
+++ inc/DanzerpressChild.php
@@ -15,11 +15,17 @@ class DanzerpressChild {
         // enqueue parent styles
         wp_enqueue_style('parent-theme', Assets\asset_path('styles/main.css'), false, null);
         
+        if (IS_DEV) {
+            $child_link = get_stylesheet_directory_uri();
+        } else {
+            $child_link = get_stylesheet_directory();
+        }
+
         // enqueue child styles
-        wp_enqueue_style('child-theme', get_stylesheet_directory_uri() . '/dist/style.min.css', ['parent-theme']);
+        wp_enqueue_style('child-theme', $child_link . '/dist/style.min.css', ['parent-theme']);
     
         //child theme js
-        wp_enqueue_script('scripts', get_stylesheet_directory_uri() . '/dist/main.min.js', array(), null, true);
+        wp_enqueue_script('scripts', $child_link . '/dist/main.min.js', array(), null, true);
     
         //google fonts
         wp_enqueue_style('child-fonts', 'https://fonts.googleapis.com/css?family=Montserrat:400,700|Roboto:400,700');	
