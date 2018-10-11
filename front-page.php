8bddac72ce4ffce768d23da982710749954406ef
bdanzer <britt.danzer@gmail.com>
2018-10-11 01:37:52 -0500

    added simple slider to front.twig

--- front-page.php
+++ front-page.php
@@ -3,7 +3,12 @@ use Danzerpress\Controllers\Controller;
 
 $context = [
     'posts' => Timber::get_posts([
-        'post_type' => 'post'
+        'post_type' => 'post',
+        'offset' => 3
+    ]),
+    'slider' => Timber::get_posts([
+        'post_type' => 'post',
+        'posts_per_page' => 3
     ]),
     'posts_per_page' => 10,
     'post_page' => 2
